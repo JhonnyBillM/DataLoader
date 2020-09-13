185bfd0e3ddcc44ba2f7646b52f84095674e766e
Jhonny Bill Mena <jhonnybillm@gmail.com>
2020-09-13 08:53:55 -0500

    UPDATE - completionFor parameters to improve the call site.
    ADD - Handler typealias to reduce “noise” when using our completion handlers

--- Sources/DataLoader/RemoteLoader.swift
+++ Sources/DataLoader/RemoteLoader.swift
@@ -10,9 +10,10 @@ import Foundation
 // TODO: add custom errors enum, if needed.
 
 class RemoteLoader {
+	typealias Handler = (Result<Data, Error>) -> ()
 
 	/// Stores the completion handlers for a given URL. This prevents to emit several download tasks for the same URL.
-	private var downloads = [String: [(Result<Data, Error>) -> ()]]()
+	private var downloads = [String: [Handler]]()
 
 	/// Downloads data from the given URL and returns `Data` or `Error` to the passed `completion` parameter.
 	///
@@ -20,7 +21,7 @@ class RemoteLoader {
 	/// - Parameters:
 	///   - url: url to download the data from.
 	///   - completion: completion handler to call with the result once the download task completes. We store this completion handler in order to prevent making several requests for the same URL.
-	func downloadData(from url: URL, completion: @escaping (Result<Data, Error>) -> ()) {
+	func downloadData(from url: URL, completion: @escaping Handler) {
 		let urlString = url.absoluteString
 		// Prevent dispatching another URL Request if we are already downloading data for the given URL.
 		// Store the completion so we can emit a response once the request completes.
@@ -35,12 +36,12 @@ class RemoteLoader {
 		// Create and handle the Download operation and response.
 		let downloadTask = URLSession.shared.dataTask(with: url) { [weak self] (data, _, error) in
 			guard error == nil else {
-				self?.completionFor(urlString: urlString, result: .failure(error!))
+				self?.completionFor(urlString, result: .failure(error!))
 				return
 			}
 			guard let data = data else { /*completion(.failure(error))*/ return }
 
-			self?.completionFor(urlString: urlString, result: .success(data))
+			self?.completionFor(urlString, result: .success(data))
 			return
 		}
 
@@ -51,7 +52,7 @@ class RemoteLoader {
 	/// - Parameters:
 	///   - urlString: key for the entry to look for in the `downloads` dictionary.
 	///   - result: result to pass down to all the handlers.
-	private func completionFor(urlString: String, result: Result<Data, Error>) {
+	private func completionFor(_ urlString: String, result: Result<Data, Error>) {
 		guard let handlers = downloads[urlString] else { return }
 		for handler in handlers {
 			handler(result)
