db8939211859ca9372f46d6209bcfdaa998cc49f
Jhonny Bill Mena <jhonnybillm@gmail.com>
2020-09-13 10:03:21 -0500

    ADD - thread safety to RemoteLoader
    Now the testDownloadDataFromMultipleThreads test succeeds

--- Sources/DataLoader/RemoteLoader.swift
+++ Sources/DataLoader/RemoteLoader.swift
@@ -15,7 +15,17 @@ class RemoteLoader {
 	/// Stores the completion handlers for a given URL. This prevents to emit several download tasks for the same URL.
 	private var downloads = [String: [Handler]]()
 
-	static let shared = RemoteLoader()
+	/// Queue to access all the internal operations to keep them thread safe.
+	private let queue: DispatchQueue
+
+	/// DataLoader singleton to manage downloading tasks with the default parameters.
+	static let shared = RemoteLoader(queue: DispatchQueue(label: "com.DataLoader.RemoteLoader", attributes: .concurrent))
+
+	/// Initializes a RemoteLoader instance with the given queue parameter.
+	/// - Parameter queue: to execute the internals operations on.
+	init(queue: DispatchQueue) {
+		self.queue = queue
+	}
 
 	/// Downloads data from the given URL and returns `Data` or `Error` to the passed `completion` parameter.
 	///
@@ -25,26 +35,30 @@ class RemoteLoader {
 	///   - completion: completion handler to call with the result once the download task completes. We store this completion handler in order to prevent making several requests for the same URL.
 	func downloadData(from url: URL, completion: @escaping Handler) {
 		let urlString = url.absoluteString
-		// Prevent dispatching another URL Request if we are already downloading data for the given URL.
-		// Store the completion so we can emit a response once the request completes.
-		guard downloads[urlString] == nil else {
-			downloads[urlString]?.append(completion)
-			return
-		}
+		queue.async(flags: .barrier) { [weak self] in
+			// Prevent dispatching another URL Request if we are already downloading data for the given URL.
+			// Store the completion so we can emit a response once the request completes.
+			guard self?.downloads[urlString] == nil else {
+				self?.downloads[urlString]?.append(completion)
+				return
+			}
 
-		// Store the completion indicating that we have one ongoing download for this URL.
-		downloads[urlString] = [completion]
+			// Store the completion indicating that we have one ongoing download for this URL.
+			self?.downloads[urlString] = [completion]
+		}
 
 		// Create and handle the Download operation and response.
 		let downloadTask = URLSession.shared.dataTask(with: url) { [weak self] (data, _, error) in
-			guard error == nil else {
-				self?.completionFor(urlString, result: .failure(error!))
+			self?.queue.async(flags: .barrier) { [weak self] in
+				guard error == nil else {
+					self?.completionFor(urlString, result: .failure(error!))
+					return
+				}
+				guard let data = data else { /*completion(.failure(error))*/ return }
+
+				self?.completionFor(urlString, result: .success(data))
 				return
 			}
-			guard let data = data else { /*completion(.failure(error))*/ return }
-
-			self?.completionFor(urlString, result: .success(data))
-			return
 		}
 
 		downloadTask.resume()
@@ -56,6 +70,8 @@ class RemoteLoader {
 	///   - result: result to pass down to all the handlers.
 	private func completionFor(_ urlString: String, result: Result<Data, Error>) {
 		guard let handlers = downloads[urlString] else { return }
+
+		// TODO: add a completionQueue parameter or something similar to let the users decide on which queue we need to return
 		for handler in handlers {
 			handler(result)
 		}
